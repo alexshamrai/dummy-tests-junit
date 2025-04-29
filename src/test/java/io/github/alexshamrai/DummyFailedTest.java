792b1d56b3f1e21b6bc6a446c82540f08baa3080
Alex Shamrai <gibsonshamray@gmail.com>
2025-04-29 15:35:28 +0300

    Implement dummy tests

---
+++ src/test/java/io/github/alexshamrai/DummyFailedTest.java
@@ -0,0 +1,20 @@
+package io.github.alexshamrai;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DummyFailedTest extends BaseFakeTest {
+
+    @Test
+    void firstFailedTest() {
+        System.out.println("DummyFailedTest firstFailedTest()");
+        assert false;
+    }
+
+    @Test
+    @DisplayName("Second failed test")
+    void secondFailedTest() {
+        System.out.println("DummyFailedTest secondFailedTest()");
+        assert false;
+    }
+}
