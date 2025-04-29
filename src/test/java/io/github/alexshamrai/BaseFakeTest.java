febb409cd46e11d53b51eb4a2c25f95013301d51
Alex Shamrai <gibsonshamray@gmail.com>
2025-04-29 15:35:28 +0300

    Implement dummy tests

---
+++ src/test/java/io/github/alexshamrai/BaseFakeTest.java
@@ -0,0 +1,34 @@
+package io.github.alexshamrai;
+
+import io.github.alexshamrai.jupiter.CtrfExtension;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(CtrfExtension.class)
+@Tag("fake")
+public abstract class BaseFakeTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BaseIntegrationTest beforeAll()");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("BaseIntegrationTest afterAll()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BaseIntegrationTest beforeEach()");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("BaseIntegrationTest afterEach()");
+    }
+}
